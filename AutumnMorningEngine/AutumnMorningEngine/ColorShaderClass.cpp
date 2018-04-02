0d8767eb9e09c7813c6b1cb7132116a6e2d89753
OSunknown <hyun1464@naver.com>
2018-04-02 21:54:36 +0900

    ColorShader ing..

--- AutumnMorningEngine/AutumnMorningEngine/ColorShaderClass.cpp
+++ AutumnMorningEngine/AutumnMorningEngine/ColorShaderClass.cpp
@@ -4,9 +4,24 @@
 
 ColorShaderClass::ColorShaderClass()
 {
+	m_vertexShader = 0;
+	m_pixelShader = 0;
+	m_layout = 0;
+	m_matrixBuffer = 0;
+}
+
+ColorShaderClass::ColorShaderClass(const ColorShaderClass & other)
+{
 }
 
 
 ColorShaderClass::~ColorShaderClass()
 {
+}
+
+bool ColorShaderClass::Initiallize(ID3D11Device * device, HWND hWnd)
+{
+	bool result;
+	result = InitializeShader(device, hWnd, L"../Engine/Color.vs", L"../Engine/Color.ps"); //<- 여기 에러뜰듯..?
+	return false;
 }
